1593f0a2c8c2f260ac16a917a64557ff9066db16
junmin-chang <chjm219@kookmin.ac.kr>
2021-02-28 00:41:13 +0900

    init

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+/* 컴포넌트를 불러온 후 
+    동일한 이름으로 내보내기 */
+
+    export { default as Main } from './Main'
+    export { default as Test } from './Test'
+    export { default as Result } from './Result'
+    
