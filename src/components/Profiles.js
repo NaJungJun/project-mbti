b95008e072c509e402e1eb23860c73df51d72b22
junmin-chang <chjm219@kookmin.ac.kr>
2021-03-02 19:53:28 +0900

    만든이들... 페이지와 돌아가기 버튼 ㅋㅋ

--- src/components/Profiles.js
+++ src/components/Profiles.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import './Profiles.scss'
-function Profiles({ name }) {
+function Profiles({ name, image, desc }) {
 
     return (
 
         
             <div className="profiles-detail">
 
-            <img src="https://i.guim.co.uk/img/media/fe1e34da640c5c56ed16f76ce6f994fa9343d09d/0_174_3408_2046/master/3408.jpg?width=620&quality=45&auto=format&fit=max&dpr=2&s=064680b85e72644d9cc2e69e2763c541" alt="profile" className="pictures"/>
+                <img src={image} alt="profile" className="pictures"/>
             
-            <span className="maker-name">{name}</span>
+                <span className="maker-name">{name}</span>
+                <div className="maker-desc">
+                    {desc}
+                </div>
+
             </div>
         
     )
--- src/pages/Makers.js
+++ src/pages/Makers.js
@@ -1,28 +1,44 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 import Profiles from '../components/Profiles';
 import '../styles/Makers.scss'
 function Makers() {
     const makers = [
         {
             id: 1,
-            name: '장준민'
+            name: '장준민(INTJ)',
+            image: 'https://post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/02/322868_1100-1100x628.jpg',
+            desc: '자기 소개'
         },
         {
             id: 2,
-            name: '박나리'
+            name: '박나리(INTJ)',
+            image: 'https://media.nature.com/lw800/magazine-assets/d41586-020-01430-5/d41586-020-01430-5_17977552.jpg',
+            desc: '자기 소개'
         },
         {
             id: 3,
-            name: '이정현'
+            name: '이정현(INTJ)',
+            image: 'https://i.guim.co.uk/img/media/684c9d087dab923db1ce4057903f03293b07deac/205_132_1915_1150/master/1915.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=14a95b5026c1567b823629ba35c40aa0',
+            desc: '자기 소개'
         }
     ]
 
     return (
         
         <div className="makers">
-            <h1>만든이들</h1>
+
+            <div style={{
+                padding: '1rem'
+            }}>
+                <h1>만든이들</h1>
+
+                <Link to="/">
+                    <button className="btn-back">돌아가기</button>
+                </Link>            
+            </div>
             {makers.map(maker => (
-                <Profiles name={maker.name}/>
+                <Profiles name={maker.name} image={maker.image} key={maker.id} desc={maker.desc}/>
             ))}
         </div>
 
