7b8c87fe916d4ab2360f16a5b7d6e796c34f5b8b
junmin-chang <chjm219@kookmin.ac.kr>
2021-03-03 22:15:06 +0900

    Share 기능

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import '../styles/Main.scss'
+import { CopyToClipboard } from 'react-copy-to-clipboard'
 function Main() {
 
+    const url = window.location.href
 
     return (
-
 
         <div className="main">
             <h2>성격으로 알아보자!</h2>
@@ -13,6 +14,10 @@ function Main() {
             <Link to="/test">
                 <button className="btn-start">시작하기</button>
             </Link>
+            <CopyToClipboard text={url} onCopy={() => alert('클립보드에 복사됨!')}>
+                                
+                <button className="btn-share">공유하기</button>
+            </CopyToClipboard>
             <Link to="/makers">
                 <button className="btn-makers">만든 사람들</button>
             </Link>
