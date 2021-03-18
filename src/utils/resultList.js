1b39c277b924395d67da42b01533c377a89dbc40
junmin-chang <chjm219@kookmin.ac.kr>
2021-03-18 16:30:54 +0900

    result list 수정

--- src/utils/resultList.js
+++ src/utils/resultList.js
@@ -2,65 +2,94 @@ import sample from '../images/sample.png'
 
 const resultList = [
     {
-        breadName: 'ㅎㅇ',
+        artistName: '살바도르 달리(ENFP)',
         description: '당신은 어떤 사람입니다\nddddddd\ndddddd',
         imgsrc: sample
     },
     {
-        breadName: '',
+        artistName: '레오나르도 다빈치(ENTP)',
         description: '당신은 어떤 사람입니다2',
         imgsrc: ''
     },
     {
-        breadName: '',
+        artistName: '빈센트 반 고흐(INFP)',
         description: '당신은 어떤 사람입니다3',
         imgsrc: ''
     },
     {
-        breadName: '',
+        artistName: '윌리엄 터너(INTP)',
         description: '당신은 어떤 사람입니다4',
         imgsrc: ''
     },
     {
-        breadName: '',
+        artistName: '앙리 마티스(ENFJ)',
         description: '당신은 어떤 사람입니다5',
         imgsrc: ''
     },
     {
-        breadName: '',
+        artistName: '클로드 모네(ENTJ)',
         description: '당신은 어떤 사람입니다6',
         imgsrc: ''
     },
     {
-        breadName: '',
+        artistName: '오딜롱 르동(INFJ)',
         description: '당신은 어떤 사람입니다7',
         imgsrc: ''
     },
     {
-        breadName: '',
+        artistName: '아널드 뵈클린(INTJ)',
         description: '당신은 어떤 사람입니다8',
         imgsrc: ''
     },
     {
-        breadName: '',
+        artistName: '조지 휴즈(ESFJ)',
         description: '당신은 어떤 사람입니다9',
         imgsrc: ''
     },
     {
-        breadName: '',
+        artistName: '엠마누엘 로이체(ESTJ)',
         description: '당신은 어떤 사람입니다10',
         imgsrc: ''
     },
     {
-        breadName: '',
+        artistName: '프레데릭 레밍턴(ESFP)',
         description: '당신은 어떤 사람입니다11',
         imgsrc: ''
     },
     {
-        breadName: '',
+        artistName: '존 제임스 오듀본(ESTP)',
         description: '당신은 어떤 사람입니다12',
         imgsrc: ''
-    }
+    },
+
+    {
+        artistName: '윈슬로 호머(ISFP)',
+        description: '당신은 어떤 사람입니다12',
+        imgsrc: ''
+    },
+
+
+    {
+        artistName: '조지아 오키프(ISTP)',
+        description: '당신은 어떤 사람입니다12',
+        imgsrc: ''
+    },
+
+
+    {
+        artistName: '노먼 록웰(ISFJ)',
+        description: '당신은 어떤 사람입니다12',
+        imgsrc: ''
+    },
+
+
+    {
+        artistName: '에드워드 호퍼(ISTJ)',
+        description: '당신은 어떤 사람입니다12',
+        imgsrc: ''
+    },
+    
+    
 ];
 
 export default resultList;
