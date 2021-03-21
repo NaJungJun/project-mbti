e430a0f0390840c1c83b3c524e541ba71178a9a8
JeongHyeon Lee <dlqjdgus99@naver.com>
2021-03-22 05:34:47 +0900

    feat: 이미지 파일 리스트 추가

---
+++ src/utils/imgList.js
@@ -0,0 +1,73 @@
+import dali_picture from '../images/dali.png';
+import leo_picture from '../images/leo.png';
+import van_picture from '../images/van.png';
+import william_picture from '../images/william.png';
+import matisse_picture from '../images/matisse.png';
+import monet_picture from '../images/monet.png';
+import ludong_picture from '../images/ludong.png';
+import arnold_picture from '../images/arnold.png';
+import george_picture from '../images/george.png';
+import emmanuel_picture from '../images/emmanuel.png';
+import frederic_picture from '../images/frederic.png';
+import john_picture from '../images/john.png';
+import homer_picture from '../images/homer.png';
+import georgia_picture from '../images/georgia.png';
+import norman_picture from '../images/norman.png';
+import hopper_picture from '../images/hopper.png';
+
+import dali_drawing from '../images/dali_drawing.png';
+import leo_drawing from '../images/leo_drawing.png';
+import van_drawing from '../images/van_drawing.png';
+import william_drawing from '../images/william_drawing.png';
+import matisse_drawing from '../images/matisse_drawing.png';
+import monet_drawing from '../images/monet_drawing.png';
+import ludong_drawing from '../images/ludong_drawing.png';
+import arnold_drawing from '../images/arnold_drawing.png';
+import george_drawing from '../images/george_drawing.png';
+import emmanuel_drawing from '../images/emmanuel_drawing.png';
+import frederic_drawing from '../images/frederic_drawing.png';
+import john_drawing from '../images/john_drawing.png';
+import homer_drawing from '../images/homer_drawing.png';
+import georgia_drawing from '../images/georgia_drawing.png';
+import norman_drawing from '../images/norman_drawing.png';
+import hopper_drawing from '../images/hopper_drawing.png';
+
+const DRAWING_LIST = {
+    dali: dali_drawing,
+    leo: leo_drawing,
+    van: van_drawing,
+    william: william_drawing,
+    matisse: matisse_drawing,
+    monet: monet_drawing,
+    ludong: ludong_drawing,
+    arnold: arnold_drawing,
+    george: george_drawing,
+    emmanuel: emmanuel_drawing,
+    frederic: frederic_drawing,
+    john: john_drawing,
+    homer: homer_drawing,
+    georgia: georgia_drawing,
+    norman: norman_drawing,
+    hopper: hopper_drawing
+};
+
+const ARTIST_LIST = {
+    dali: dali_picture,
+    leo: leo_picture,
+    van: van_picture,
+    william: william_picture,
+    matisse: matisse_picture,
+    monet: monet_picture,
+    ludong: ludong_picture,
+    arnold: arnold_picture,
+    george: george_picture,
+    emmanuel: emmanuel_picture,
+    frederic: frederic_picture,
+    john: john_picture,
+    homer: homer_picture,
+    georgia: georgia_picture,
+    norman: norman_picture,
+    hopper: hopper_picture
+};
+
+export {ARTIST_LIST, DRAWING_LIST};
