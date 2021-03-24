6bedf1d71e8b73ad7e944551c837fe09fc5ca00c
junmin-chang <chjm219@kookmin.ac.kr>
2021-03-24 14:43:53 +0900

    가로 모드 방지

--- src/common/MainWrapper.js
+++ src/common/MainWrapper.js
@@ -5,11 +5,12 @@ import './MainWrapper.scss'
 function MainWrapper({ children }) {
 
     return (
-
+        <>
+        <div id="landscape">화면을 세로 모드로 해주세요~~</div>
         <div className="main-wrapper">
             {children}
         </div>
-
+        </>
     )
     
 }
