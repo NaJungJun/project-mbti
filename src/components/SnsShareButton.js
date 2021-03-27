cd56520e99998c5d0966ba3343bfbd92c4fe7e9a
NariP <tto9208@naver.com>
2021-03-27 11:55:19 +0900

    1-snsShare : snsShareButton 관련 오류 수정, kakao 공유 합치기

--- src/components/SnsShareButton.js
+++ src/components/SnsShareButton.js
@@ -1,27 +1,68 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { ICON_LIST } from '../utils/imgList';
+import { SNS_NAME } from '../utils/plainText';
 
-function SnsShareButton ( { props } ) {
+function SnsShareButton ( { name, imgSrc, artistName } ) {
+    const { KAKAO, TWITTER, FACEBOOK } = SNS_NAME;
+    const {REACT_APP_KAKAO_KEY, OUR_SITE_URL} = process.env;
     const myUrl = encodeURIComponent('https://mbti-artist.netlify.app');
-    const name = props.name;
-    const className = name+'-share-btn';
-    let snsUrl;
+    const TWITTER_BASE_URL = 'https://twitter.com/intent/tweet?text=[TEXT]&url=';
+    const FACEBOOK_BASE_URL = 'http://www.facebook.com/sharer/sharer.php?u=';
+
+
+    const sendKakaoLink = () => {
+        window.Kakao && !window.Kakao.isInitialized() && window.Kakao.init(REACT_APP_KAKAO_KEY);
+        window.Kakao.Link.sendDefault({
+            objectType: 'feed',
+            content: {
+                title: artistName,
+                description: '나와 비슷한 화가가 누구인지 궁금하다면~??',
+                imageUrl: imgSrc,
+                link: {
+                    mobileWebUrl: 'https://mbti-artist.netlify.app',
+                    webUrl: 'https://mbti-artist.netlify.app',
+                },
+            },
+            buttons: [
+                {
+                    title: '테스트 하러 가기 ♬',
+                    link: {
+                        mobileWebUrl: 'https://mbti-artist.netlify.app',
+                        webUrl: 'https://mbti-artist.netlify.app',
+                    },
+                },
+            ],
+        });
+    }
 
     const setSnsUrl = () => {
-        if (name==='twitter') { snsUrl = 'https://twitter.com/intent/tweet?text=[TEXT]&url=' + myUrl; }
-        else if (name==='facebook') { snsUrl = 'http://www.facebook.com/sharer/sharer.php?u=' + myUrl; }
-    }
+        // 각 sns 별 공유할 주소 세팅하는 함수
+        switch (name) {
+            case TWITTER:
+                return `${TWITTER_BASE_URL}${myUrl}`;
+            case FACEBOOK:
+                return `${FACEBOOK_BASE_URL}${myUrl}`;
+            default:
+                !name && console.warn(`undefined props: name`);
+                break;
+        }
+    };
 
     const onClick = () => {
-        setSnsUrl();
-        console.log(snsUrl)
-        window.open(snsUrl);
-    }
+        // facebook, twitter 공유하기 기능
+
+        window.open(setSnsUrl(), '_blank');
+    };
+
+    useEffect(()=>{
+        window.Kakao && !window.Kakao.isInitialized() && window.Kakao.init(process.env.REACT_APP_KAKAO_KEY);
+    },);
     
-    return(
-        <a href="#" onClick={onClick} target="_blank" className={className}>
-            <img src={ICON_LIST[name]} className="icon-img"/>
-        </a>);
+    return (
+           <button className='sns-share-btn' onClick={ (name === KAKAO) ? sendKakaoLink : onClick }>
+               <img src={ ICON_LIST[name] } className="icon-img" alt={ `${name} share button` } />
+           </button>
+    );
 }
 
 export default SnsShareButton;
