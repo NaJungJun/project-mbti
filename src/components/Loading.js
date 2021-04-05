ea0c6ceb0a925732b8cd3f9937042e7ea5d84ccc
NariP <tto9208@naver.com>
2021-04-06 03:45:23 +0900

    2-loadingAnimation : modify loading animation bug

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -5,7 +5,7 @@ import '../styles/Loading.scss';
 function Loading() {
     return (
         <div>
-            <span>결과를 기다리는 중...</span>
+            <span style={{display: 'block', fontSize: '1.3rem'}}>결과를 기다리는 중...</span>
             <img src={ loading } alt='loading animation'/>
         </div>
     );
